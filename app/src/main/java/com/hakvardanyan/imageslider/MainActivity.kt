8bf89d4291c70b23bbf727a7a12657b95314f20f
hakobvardanyan <hakvardanyan@gmail.com>
2022-10-16 12:46:08 +0300

    initial commit of project files

---
+++ app/src/main/java/com/hakvardanyan/imageslider/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hakvardanyan.imageslider
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
