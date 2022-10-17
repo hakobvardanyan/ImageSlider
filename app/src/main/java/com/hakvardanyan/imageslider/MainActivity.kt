c25012f20d2680114e3a60dc6793125ad83564e0
hakobvardanyan <hakvardanyan@gmail.com>
2022-10-17 10:59:41 +0300

    Added view binding

--- app/src/main/java/com/hakvardanyan/imageslider/MainActivity.kt
+++ app/src/main/java/com/hakvardanyan/imageslider/MainActivity.kt
@@ -11,9 +11,11 @@ import androidx.constraintlayout.helper.widget.Carousel
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.transition.Transition
+import com.hakvardanyan.imageslider.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private val images = arrayOf(
         "https://images.unsplash.com/photo-1624976172336-54d765427b6b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80",
         "https://images.unsplash.com/photo-1584060622420-0673aad46076?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80",
@@ -29,38 +31,37 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setupCarousel()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupSlider()
     }
 
-    private fun setupCarousel() {
-        val carousel = findViewById<Carousel>(R.id.carousel) ?: return
+    private fun setupSlider() {
 
-        carousel.setAdapter(object : Carousel.Adapter {
+        binding.slider.setAdapter(object : Carousel.Adapter {
 
             override fun count(): Int = images.size
 
             override fun populate(view: View, index: Int) {
                 val imageView = (view as? ViewGroup)?.getChildAt(0)
                 if (imageView is AppCompatImageView) {
-
                     Glide.with(view)
                         .asBitmap()
                         .load(images[index])
-                        .into(object : CustomTarget<Bitmap?>() {
-                            override fun onResourceReady(
-                                resource: Bitmap,
-                                transition: Transition<in Bitmap?>?
-                            ) {
-                                imageView.setImageBitmap(resource)
-                            }
-
-                            override fun onLoadCleared(placeholder: Drawable?) = Unit
-                        })
+                        .into(createCustomTarget(imageView))
                 }
             }
 
             override fun onNewItem(index: Int) = Unit
         })
+    }
+
+    private fun createCustomTarget(view: AppCompatImageView) = object : CustomTarget<Bitmap?>() {
+
+        override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap?>?) {
+            view.setImageBitmap(resource)
+        }
+
+        override fun onLoadCleared(placeholder: Drawable?) = Unit
     }
 }
